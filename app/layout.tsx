05e6411bee2cbf00fd9d4407a93f9e7783742e6e
Kartik Agarwal <kartik_a@mt.iitr.ac.in>
2025-05-16 16:49:49 +0530

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://chat.dietify.in'),
+  title: 'Chat with me',
+  description: 'Chat with me',
 };
 
 export const viewport = {
